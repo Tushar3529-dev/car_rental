ecc10baff76b3891702c07e61bf52cce6d12a6d7
Tushar Yadav <tusharyadav3529@gmail.com>
2025-09-02 20:46:39 +0530

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,13 @@ import 'package:car_rental/presentation/bloc/car_bloc.dart';
 import 'package:car_rental/presentation/pages/onboarding_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // Hide both status bar (top) and navigation bar (bottom)
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   initInjection();
   runApp(const MyApp());
